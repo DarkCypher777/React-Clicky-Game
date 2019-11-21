a434e47f9d535487c41f88a45acc5015a0f997da
Ben Diamond <54002015+DarkCypher777@users.noreply.github.com>
2019-11-20 19:34:28 -0500

    pictures added and code updated

--- src/images.js
+++ src/images.js
@@ -1,17 +1,53 @@
-import name from "./images/";
-import name from "./images/";
-import name from "./images/";
-import name from "./images/";
-import name from "./images/";
-import name from "./images/";
-import name from "./images/";
-import name from "./images/";
-import name from "./images/";
-import name from "./images/";
-
+import bulbasur from "./images/bulbasur.png";
+import abra from "./images/abra.png";
+import bellsprout from "./images/bellsprout.png";
+import caterpie from "./images/caterpie.png";
+import chamander from "./images/chamander.png";
+import dratini from "./images/dratini.png";
+import eevee from "./images/eevee.png";
+import jigglypuff from "./images/jigglypuff.png";
+import mankey from "./images/mankey.png";
+import megaball from "./images/megaball.png";
+import meowth from "./images/meowth.png";
+import mew from "./images/mew.png";
+import pidgey from "./images/pidgey.png";
+import pikachu from "./images/pikachu.png";
+import pokeball from "./images/pokeball.png";
+import psyduck from "./images/psyduck.png";
+import rattata from "./images/rattata.png";
+import snorlax from "./images/snorlax.png";
+import squirtle from "./images/squirtle.png";
+import superball from "./images/superball.png";
+import ultraball from "./images/ultraball.png";
+import venonat from "./images/venonat.png";
+import weedle from "./images/weedle.png";
+import zubat from "./images/zubat.png";
 
 const images = [
-
+    abra,
+    bulbasur,
+    bellsprout,
+    caterpie,
+    chamander,
+    dratini,
+    eevee,
+    jigglypuff,
+    mankey,
+    megaball,
+    meowth,
+    mew,
+    pidgey,
+    pikachu,
+    pokeball,
+    psyduck,
+    rattata,
+    snorlax,
+    squirtle,
+    superball,
+    ultraball,
+    venonat,
+    weedle,
+    zubat
 ];
 
 export default images;
